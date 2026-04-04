c54edf98d62aff868031f10e50e0da1dad7daffa
AnshulRoy28 <royanshul5002@gmail.com>
2026-04-04 13:50:29 +0530

    Add automatic database seeding on startup

--- app/database.py
+++ app/database.py
@@ -6,3 +6,7 @@ def init_db(app):
     db.init_app(app)
     with app.app_context():
         db.create_all()
+        
+        # Auto-seed on first run
+        from seed import seed_database_if_empty
+        seed_database_if_empty()
